56e001e9e846b777c74ea29d043cbef124b3df0a
Rumeysa Layık <37456977+rumeysalyk@users.noreply.github.com>
2021-08-04 10:56:57 +0300

    Console Programlama dosyası eklendi

---
+++ console-programlama/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            Console.WriteLine("İsminizi giriniz: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı giriniz: ");
+            string surname = Console.ReadLine();
+            
+            Console.WriteLine("Hello "+ name + " " + surname);
+
+        }
+    }
+}
