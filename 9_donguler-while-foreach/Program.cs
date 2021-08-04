605766db18d9b74ae7e4d48efb8ca9af2f6262cf
Rumeysa Layık <37456977+rumeysalyk@users.noreply.github.com>
2021-08-05 00:51:25 +0300

    Add files via upload

---
+++ 9_donguler-while-foreach/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _9_donguler_while_foreach
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+            int counter=1, sum=0;
+
+            while(counter<number)
+            {
+                sum += counter;
+                counter++;
+            }
+
+            Console.WriteLine(sum/counter);
+
+            char c = 'a';
+            while(c<'z')
+            {
+                Console.WriteLine(c);
+                c++;
+            }
+
+            Console.WriteLine("*************** Foreach ***************");
+
+            string[] cars = {"BMV","Ford","Toyota","Nissan"};
+
+            foreach(var item in cars)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
