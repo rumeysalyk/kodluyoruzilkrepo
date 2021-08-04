0e10af23b4e8327028314b50d33b0bde07e7d399
Rumeysa Layık <37456977+rumeysalyk@users.noreply.github.com>
2021-08-04 22:34:10 +0300

    İf/Elseif/Else

---
+++ if-elseif/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace if_elseif
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if(time>6 && time<=11)
+                Console.WriteLine("Günaydin");
+            else if(time<18)
+                Console.WriteLine("İyi Günler");
+            else
+                Console.WriteLine("İyi Geceler");
+
+            //***************Second way***************
+            string sonuc = time>=6 && time<=11 ? "Günaydın":time <=18 ? "İyi Günler":"İyi Geceler";
+            Console.WriteLine(sonuc);
+
+        }
+    }
+}
