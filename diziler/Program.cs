55fe7061abb0df4030d4004683fc6f4524002767
Rumeysa Layık <37456977+rumeysalyk@users.noreply.github.com>
2021-08-05 12:32:35 -0700

    Add files via upload

---
+++ diziler/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace diziler
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] renkler = new string[5];
+            string[] hayvanlar = {"Kedi","Köpek","Kuş","Maymun"};
+            
+            int[] dizi;
+            dizi = new int[5];
+            
+            //dizilere değer ataması 
+            renkler[0] = "Mavi";
+            dizi[3] = 10;
+            
+            Console.WriteLine(hayvanlar[1]);
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(renkler[0]);
+            
+            Console.WriteLine("Lütfen dizinin eleman sayısını giriniz: ");
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            
+            int[] sayiDizisi = new int[diziUzunlugu];
+            
+            for(int i=0;i<diziUzunlugu;i++)
+            {
+                Console.Write("Lutfen {0}.eleman sayisini giriniz: ",i+1);
+                sayiDizisi[i] = int.Parse(Console.ReadLine());
+            }
+
+            double toplam = 0;
+
+            foreach (var sayi in sayiDizisi)
+                toplam += sayi;
+            
+            Console.WriteLine("Ortalama: "+ toplam/diziUzunlugu);
+        }
+    }
+}
